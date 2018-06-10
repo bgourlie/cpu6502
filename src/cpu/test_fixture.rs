07000dc1ef2f117f8f913f665bb9ecf24c90d225
W. Brian Gourlie <bgourlie@gmail.com>
2018-06-10 17:36:25 -0500

    Initial Commit

---
+++ src/cpu/test_fixture.rs
@@ -0,0 +1,56 @@
+use cpu::Cpu;
+
+use cpu::{Interconnect, Interrupt, ADDRESSABLE_MEMORY};
+
+pub struct TestInterconnect {
+    addr: [u8; ADDRESSABLE_MEMORY],
+}
+
+impl TestInterconnect {
+    pub fn new() -> Self {
+        TestInterconnect {
+            addr: [0; ADDRESSABLE_MEMORY],
+        }
+    }
+}
+
+impl Default for TestInterconnect {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl Interconnect for TestInterconnect {
+    fn read(&self, addr: u16) -> u8 {
+        let addr = addr as usize;
+        self.addr[addr]
+    }
+
+    fn write(&mut self, addr: u16, data: u8) {
+        let addr = addr as usize;
+        self.addr[addr] = data;
+    }
+
+    fn tick(&mut self) -> Interrupt {
+        Interrupt::None
+    }
+}
+
+pub type TestCpu = Cpu<TestInterconnect>;
+
+impl Cpu<TestInterconnect> {
+    pub fn new_test() -> Self {
+        let interconnect = TestInterconnect::default();
+        Cpu::new(interconnect, 0x200)
+    }
+
+    pub fn write(&mut self, addr: u16, value: u8) {
+        self.interconnect.write(addr, value);
+    }
+
+    pub fn store_many(&mut self, addr: u16, data: &[u8]) {
+        for (i, byte) in data.iter().enumerate() {
+            self.interconnect.write(addr + i as u16, *byte);
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(unknown_lints)]
+#![allow(cast_lossless)]
+
+#[cfg(test)]
+extern crate asm6502;
+
+#[cfg(test)]
+extern crate rand;
+
+mod byte_utils;
+pub mod cpu;
